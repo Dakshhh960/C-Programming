7b7e6b51d06b71dc88516bc586bcc81651f5c1ba
Dakshhh960 <daksh.kumar96086@gmail.com>
2026-09-23 23:40:03 +0530

    Create 03_average_of_three_number.c

---
+++ 03_average_of_three_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+##include<math.h>
+int main ()[
+int a,b,c,avg;
+printf("enter the number a: ");
+scanf("%f",&a);
+printf("enter the number b: ");
+scanf("%f",&b);
+printf("enter the number c: ");
+scanf("%f",&c);
+avg=(a+b+c)/3;
+printf("avg is =%f,avg);
+return 0;
+}
